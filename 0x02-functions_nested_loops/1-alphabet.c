8f4cb699f689ce978fbf86a4fc94024dee091c48
wise-dom <ange.behi19@gmail.com>
2023-08-18 12:06:00 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include "main.h"
+
 /*
- * print_alphabet : prints the alphabet in lowercase
- * 
- * Return 0
+ *print_alphabet: prints the alphabet in lowercase
+ *
+ * Return void
  */
 
 void print_alphabet(void)
 {
-	int alphabet;
+	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
@@ -15,3 +17,5 @@ void print_alphabet(void)
 
 	return (0);
 }
+ 
+
